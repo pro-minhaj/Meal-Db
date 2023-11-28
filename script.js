df66647813ded0e06ce3f40b1b2c9e8af9208794
Md Minhaj <parsonal322532@gmail.com>
2023-11-28 12:05:03 +0600

    Food Dynamic Added

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const loadData = async () =>{
-    const res = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
+const loadData = async (search = 'fish') =>{
+    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
     const data = await res.json()
     showDisplay(data.meals)
 }
 
 const showDisplay = datas => {
     const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = '';
     datas.forEach(element => {
-        console.log(element.strInstructions);
+        // console.log(element.idMeal);
         const newElement = document.createElement('div');
         newElement.classList.add('col-md-6');
         newElement.innerHTML = `
@@ -20,7 +21,7 @@ const showDisplay = datas => {
                 <div class="card-body">
                     <h5 class="card-title">${element.strMeal}</h5>
                     <p class="card-text">${element.strInstructions}</p>
-                    <button type="button" class="btn p-0 text-warning details-btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    <button onclick="showDetails(${element.idMeal})" type="button" class="btn p-0 text-warning details-btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                     View Details
                     </button>
                 </div>
@@ -30,7 +31,25 @@ const showDisplay = datas => {
         `;
         cardContainer.appendChild(newElement);
     });
-    
+}
+
+const searchArea = () =>{
+    const searchFilud = document.getElementById('search-filud').value;
+    loadData(searchFilud);
+}
+
+const showDetails = (idMeal)=>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => mobail(data.meals[0]))
+}
+
+const mobail = (data)=>{
+    const mobailTitle = document.getElementById('staticBackdropLabel');
+    mobailTitle.innerText = data.strMeal;
+    const modailBody = document.getElementById('modail-body');
+    modailBody.innerText = data.strInstructions;
 }
 
 loadData();
